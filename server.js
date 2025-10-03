b8891051ff11813126a3e87b57156113a8b0bff7
ryograhhh <ryoevisu@gmail.com>
2025-10-03 17:29:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.post("/api/get_reactions", async (req, res) => {
