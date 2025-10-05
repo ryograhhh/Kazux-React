5e97806ad654281497679325f37866a9b9f5a2b7
ryograhhh <ryoevisu@gmail.com>
2025-10-05 15:23:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,39 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.post("/api/get_profile", async (req, res) => {
+  try {
+    const { cookie } = req.body;
+    
+    const userIdMatch = cookie.match(/c_user=(\d+)/);
+    if (!userIdMatch) {
+      return res.json({ success: false, error: "Invalid cookie format" });
+    }
+    
+    const userId = userIdMatch[1];
+    
+    const profileUrl = `https://graph.facebook.com/${userId}?fields=name,picture.type(large)&access_token=${userId}|placeholder`;
+    
+    try {
+      const response = await axios.get(profileUrl);
+      res.json({
+        success: true,
+        name: response.data.name,
+        avatar: response.data.picture?.data?.url || `https://graph.facebook.com/${userId}/picture?type=large`
+      });
+    } catch (error) {
+      res.json({
+        success: true,
+        name: userId,
+        avatar: `https://graph.facebook.com/${userId}/picture?type=large`
+      });
+    }
+  } catch (error) {
+    console.error("Error fetching profile:", error.message);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+});
+
 app.post("/api/get_reactions", async (req, res) => {
   try {
     const { session, link: post_link, reaction_type } = req.body;
